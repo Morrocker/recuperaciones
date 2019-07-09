870f4602f91847cf496efbe72a72c284cccbd3e0
Morrocker <i.romero.hinrich@gmail.com>
2019-07-09 02:11:23 -0400

    asd

--- src/app/requests.service.ts
+++ src/app/requests.service.ts
@@ -60,7 +60,7 @@ export class RequestsService {
   sendTempRequest(recepcion: Delivery, userId: number): Observable<number> {
     return this.http
     // .post<number>(`localhost:5050/cloner/recepcion/${userId}`, recepcion, httpOptions)
-    .post<number>(`localhost:5050/cloner/recepcion`, recepcion, httpOptions)
+    .post<number>(`http://localhost:5050/cloner/recepcion`, recepcion, httpOptions)
     .pipe(
       catchError(this.handleError<number>('newRequest'))
     );
